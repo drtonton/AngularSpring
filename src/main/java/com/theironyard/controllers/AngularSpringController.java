1d9ee101aae7d13eb4e7dcdd1b4a6fcd6aa8c7a7
drtonton <tkfoster26@gmail.com>
2016-03-16 10:16:55 -0400

    initial import

---
+++ src/main/java/com/theironyard/controllers/AngularSpringController.java
@@ -0,0 +1,11 @@
+package com.theironyard.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by noellemachin on 3/16/16.
+ */
+@RestController
+public class AngularSpringController {
+
+}
